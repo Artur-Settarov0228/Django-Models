9043af8a86927a50cac77faa653df0af672f3e65
settarovartur <settarovartur2@gamil.com>
2025-11-07 18:30:23 +0500

    init

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    gender = {
+        '1': 'male',
+        '2': 'famale',
+    }
+
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    gender_type = models.CharField(choices=gender.items(), null = False)
+    
+    def __str__(self):
+        return f" {id} - {self.name}"
+
+
